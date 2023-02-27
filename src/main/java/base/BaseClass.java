3469f68e53cc39f28f7afba92d1e94ef10d74826
tejastoley <tejas.toley@impactguru.com>
2023-02-27 17:02:23 +0530

    test

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -13,26 +13,25 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	public static WebDriver driver;
-    public static Properties prop;
+
     String path = System.getProperty("user.dir");
     
-    public BaseClass() throws IOException, IOException {
-        prop = new Properties();
-        FileInputStream ip = new FileInputStream(path+"\\src\\main\\java\\config\\config.properties");
-        prop.load(ip);
-    }
+
     
     public static void initialization() {
 
 
     	ChromeOptions option = new ChromeOptions();
-    	option.setHeadless(false);
+    	option.setHeadless(true);
 
-    	WebDriverManager.chromedriver().setup();
+        //
+        ///usr/bin/chromedriver
+
+    	System.setProperty("webdriver.chrome.driver","C:\\Users\\impac\\Desktop\\selenium\\chromedriver.exe");
         driver = new ChromeDriver(option);
     	
         driver.manage().window().maximize();
-        driver.get(prop.getProperty("url"));
+        driver.get("https://www.guru99.com/software-testing.html");
         driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
    }
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -14,7 +14,7 @@ public class HomePage extends BaseClass {
 	WebElement testing;
 	
 	
-	public HomePage() throws IOException, IOException {
+	public HomePage(){
 		super();
 		// TODO Auto-generated constructor stub
 		PageFactory.initElements(driver,this);
