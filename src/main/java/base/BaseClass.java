fbaf0bf80e635d0d33529ffb57c20a752844fe64
Tejas <46339378+tejast06@users.noreply.github.com>
2023-03-14 16:01:44 +0530

    Update BaseClass.java

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -26,7 +26,7 @@ public class BaseClass {
 
 
     	ChromeOptions option = new ChromeOptions();
-    	option.addArguments("--headless");
+    	option.addArguments("-headless");
         option.addArguments("--no-sandbox");
 	option.addArguments("--disable-dev-shm-usage");
     	System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
